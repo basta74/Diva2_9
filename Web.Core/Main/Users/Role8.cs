268b28b648380eb53f04a47e68b8aa6b4fe21450
bastinec <pc@DESKTOP-EHBELOS>
2026-05-10 11:03:53 +0200

    Align rozvrh

--- Web.Core/Main/Users/Role8.cs
+++ Web.Core/Main/Users/Role8.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+
+using Microsoft.AspNetCore.Identity;
 
 namespace Diva2.Core.Main.Users
 {
